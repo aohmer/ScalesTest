2bb78ba6119734d7bb0204f636e07573ffdf5477
aohmer <aaron.ohmer@gmail.com>
2026-01-24 23:21:43 -0500

    added to app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def ScalesTest():
-    return "<h1>Scales Test</h1>"
+    return "<h1>Scales Test</h1>" + "<p>This is a test page for the Scales application.</p>"
